eac7434b8e78a2f59007e73707208cf0cdfe0362
Karen Smith <karensmith@Karens-Air.local>
2019-02-06 19:31:16 -0800

    final game

--- js/main.js
+++ js/main.js
@@ -1,72 +1,195 @@
-// start game - clear board
-// getWinner
-//switch turns 
-//drop pieces upon click
 
-// const 
+
 var currentPlayer;
-const playerArray = [];
 
-const winCombo = [[35, 28, 21, 14], [28, 21, 14, 7], [21,14,7,0], [36,29,22,15],
-                [29,22,15,8], [22,15,8,1], [2,9,16,23], [9,16,23,30],[16,23,30,37],[3,10,17,24], [10,17,24,31], [17,24,31, 38], 
-                [4,11,18,25], [11,18,25,32], [18,25,32,39],[5,12,19,26], [12,19,26,33],[19,26,33,40], [6,13,20,27], [13,20,27,34],
-                [20,27,34,41], [0,1,2,3], [1,2,3,4],[2,3,4,5], [3,4,5,6],[7,8,9,10], [8,9,10,11], [9,10,11,12], [10,11,12,13],
-                [14,15,16,17],[15,16,17,18],[16,17,18,19],[17,18,19,20], [21,22,23,24], [22,23,24,25,], [23,24,25,26], [24,25,26,27],
-                [28,29,30,31], [29,30,31,32], [30,31,32,33], [31,32,33,34],[35,36,37,38], [36,37,38,39], [37,38,39,40], [38,39,40,41],
-                [21,15,9,3], [28,22,16,10],[22,16,10,4], [35,29,23,17], [29,23,17,11], [23,17,11,5],[36,30,24,18], [30,24,18,12],[24,18,12,6],
-                [37,31,25,19], [31,25,19,13], [38,32,26,20],[3,11,19,27], [2,10,18,26], [10,18,26,34],[1,9,17,25], [9,17,25,33], [17,25,33,41],
-                [40,32,24,16], [32,24,16,8], [24,26,8,0], [7,15,23,31], [15,23,31,39],[38,30,22,14]];
-// cached element references 
+var winner = null
+
+var p1 = [];
+var p2 = [];
+
+document.querySelector("table").addEventListener("click", handleMove);
 
 
-// evt listeners
-document.getElementById("ball").addEventListener("mouseover", playerTurn);
-// document.getElementById("ball").addEventListener("mouseover", trumpTurn);
+const winCombo = [[0,6,12,18], [6,12,18,24], [12,18,24,30], [18,24,30,36],
+                [1,7,13,19], [7,13,19,25], [13,19,25,31], [19,25,31,37],
+                [2,8,14,20], [8,14,20,26], [14,20,26,32], [20,26,32,38],
+                [3,9,15,21], [9,15,21,27], [15,21,27,33], [21,27,33,39],
+                [4,10,16,22], [10,16,22,28],[16,22,28,34], [22,28,34,40],
+                 [5,11,17,23],[11,17,23,29], [17,23,29,35], [23,29,35,41],
+                 [0,1,2,3], [1,2,3,4], [2,3,4,5],[6,7,8,9], [7,8,9,10], [8,9,10,11],
+                [12,13,14,15], [13,14,15,16], [14,15,16,17], [18,19,20,21],[19,20,21,22], [20,21,22,23],
+                 [24,25,26,27], [25,26,27,28], [26,27,28,29], [30,31,32,33],[31,32,33,34],[32,33,34,35],
+                 [36,37,38,39], [37,38,39,40], [38,39,40,41], [23,28,33,38], [17,22,27,32], [22,27,32,37],
+                [11,16,21,26], [16,21,26,31], [21,26,31,36],[5,10,15,20], [10,15,20,25],[15,20,25,30],
+                [4,9,14,19], [9,14,19,24], [3,8,13,18], 
+                [2,9,16,23], [1,8,15,22], [8,15,22,29], [0,7,14,21], [7,14,21,28], [14,21,28,35],
+                [6,13,20,27], [13,20,27,34],[20,27,34,41], [12,19,26,33], [19,26,33,40],
+                 [18,25,32,39]];
 
-// document.getElementById("ball").addEventListener("mouseout", );
-  //create hover over with russian and american flags
-// functions
+function getWinner() {
+    for(var i = 0; i < winCombo.length; i++) {
 
-// $('#ball').mouseOver(function(){
-//     $('#ball').css('backgroundColor', blue);
-// })
+            if(p1.includes(winCombo[i][0]) 
+                && p1.includes(winCombo[i][1]) 
+                && p1.includes(winCombo[i][2]) 
+                && p1.includes(winCombo[i][3]) 
+                ){
+                var winMessage = document.getElementById("message");
+                winMessage.innerHTML = "LOCK HIM UP!<br> MUELLER WON!";
+                winMessage.style.display = "flex"
+
+                winner = 'p1'
+                gameover=true;
+            }
+            if(p2.includes(winCombo[i][0]) 
+                && p2.includes(winCombo[i][1]) 
+                && p2.includes(winCombo[i][2]) 
+                && p2.includes(winCombo[i][3]) 
+                ){
+                document.getElementById("message")
+                var winMessage = document.getElementById("message");
+                winMessage.innerHTML = "NO 1 IS BETER <br> AT CONEKT 4 <br> THAN ME! SAD!";
+                winMessage.style.display = "flex"
+
+                winner = 'p2'
+                gameover = true;
+            }
+        // }
+    }
+    return "hello"
+
+}
+
 
 init();
 
 function init () {
-     currentPlayer = 0;
+     
 } 
-// if cell is empty, store current player move into an array
-// check for winner, if no winner, switch turn 
 
- 
-var x = 0;
-var y = 0;
-
+currentPlayer = 0;
 function playerTurn () {
     currentPlayer = currentPlayer % 2;
-    if(currentPlayer === 0 ){
-        document.getElementById("ball").style.backgroundColor = "green";
-    }else{
-        document.getElementById("ball").style.backgroundColor = "red";
+    if(currentPlayer === 0 ) {
+        currentPlayer++;
+        return "url(\"images/mueller.jpg\")";
+    }else if(currentPlayer === 1){
+        currentPlayer++;
+        return "url(\"images/trump.jpeg\")";
     }
-    // console.log(target.id);
-    
-    currentPlayer++;
-  
+}
+var gameover = false;
+var counter = 0
+function handleMove(evt) {
+    // obtain index of square
+    var clickAbles = [0,6,12,18,24,30,36];
+    var idx = Number(evt.target.id);
+    if (clickAbles.indexOf(idx) !== -1 && gameover===false) { ``
+        
+        // var move = evt.target.closest("tr").lastElementChild;
+        console.log(document.getElementById(`${parseInt(idx) + 5}`).style.background === '')
+        
+        if(document.getElementById(`${parseInt(idx) + 5}`).style.background === ''){
+            document.getElementById(`${parseInt(idx) + 5}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx) + 5}`).style.backgroundSize = "contain";
+            // document.getElementById(`${parseInt(idx) + 5}`).appendChild(`
+            //    
+            // `)
+            if(counter % 2 === 0){
+                p1.push(parseInt(idx) + 5)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(parseInt(idx) + 5)
+                getWinner()
+                counter++
+            }
+        }
+        else if(document.getElementById(`${parseInt(idx) + 4}`).style.background === ''){
+            document.getElementById(`${parseInt(idx) + 4}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx) + 4}`).style.backgroundSize = "contain";
+            if(counter % 2 === 0){
+                p1.push(parseInt(idx) + 4)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(parseInt(idx) + 4)
+                getWinner()
+                counter++
+            }
+        }
+        else if(document.getElementById(`${parseInt(idx) + 3}`).style.background === ''){
+            document.getElementById(`${parseInt(idx) + 3}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx) + 3}`).style.backgroundSize = "contain";
+            if(counter % 2 === 0){
+                p1.push(parseInt(idx) + 3)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(parseInt(idx) + 3)
+                getWinner()
+                counter++
+            }
+        }
+        else if(document.getElementById(`${parseInt(idx) + 2}`).style.background === ''){
+            document.getElementById(`${parseInt(idx) + 2}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx) + 2}`).style.backgroundSize = "contain";
+            if(counter % 2 === 0){
+                p1.push(parseInt(idx) + 2)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(parseInt(idx) + 2)
+                getWinner()
+                counter++
+            }
+        }
+        else if(document.getElementById(`${parseInt(idx) + 1}`).style.background === ''){
+            document.getElementById(`${parseInt(idx) + 1}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx) + 1}`).style.backgroundSize = "contain";
+            if(counter % 2 === 0){
+                p1.push(parseInt(idx) + 1)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(parseInt(idx) + 1)
+                getWinner()
+                counter++
+            }
+        }
+        else if(document.getElementById(`${parseInt(idx)}`).style.background === ''){
+            document.getElementById(`${parseInt(idx)}`).style.background = playerTurn();
+            document.getElementById(`${parseInt(idx)}`).style.backgroundSize = "contain";
+            if(counter % 2 === 0){
+                p1.push(idx)
+                getWinner()
+                counter++
+            }
+            else {
+                p2.push(idx)
+                getWinner()
+                counter++
+            }
+        }
+        else {
+            alert("not a valid move!")
+        }    
+        console.log(counter)
+        
+    } 
 }
 
-
- 
-
-
-
-
-// function getWinner(idx) {
-//     if (idx === "mueller" && idx - 1 === "mueller" && idx -2 === "mueller" && idx - 3 ==="mueller")
-//     console.log("you've made america great again")
-// }
-
+    
+    function clickedButton () { 
+            location.reload();
+            }
+        
     
 
-    //store player click in array - test array against winning combos array to check for winner
+  
+
